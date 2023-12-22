ee18c6271a0c96432e4778e6c69f9599e018c6fb
Domitory1 <jomunyts@gmail.com>
2023-12-23 02:08:05 +0300

    new file:   Bot/Buttons/__pycache__/button.cpython-39.pyc new file:   Bot/Buttons/__pycache__/buttons.cpython-39.pyc new file:   Bot/Buttons/buttons.py new file:   Bot/bot.py

---
+++ Bot/Buttons/buttons.py
@@ -0,0 +1,10 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+
+
+application_number_phone = ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[
+   [KeyboardButton(text="Поделиться номером", request_contact=True)]
+])
+
+inline_menu = InlineKeyboardMarkup(row_width=1, inline_keyboard=[
+   [InlineKeyboardButton(text="Меню", web_app=WebAppInfo(url='https://domitory1.github.io/index.html'))]
+])
